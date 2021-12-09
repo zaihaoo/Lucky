78b397356df75fd35d0f23f768afa9c58101c52e
zaihaoo <lizihao940412@gmail.com>
2021-12-09 14:08:36 +0800

    feat: 素描纹理填充

--- src/shader/cube.fs
+++ src/shader/cube.fs
@@ -1,5 +1,48 @@
 precision mediump float;
-varying vec4 v_color;
+varying vec2 vUv;
+uniform sampler2D texture;
+uniform sampler2D hatch0;
+uniform sampler2D hatch1;
+uniform sampler2D hatch2;
+
+float shade(const in float shading, const in vec2 uv) {
+  float shadingFactor;
+  float stepSize = 1.0 / 3.0;
+
+  float alpha = 0.0;
+  float scaleWhite = 0.0;
+  float scaleHatch0 = 0.0;
+  float scaleHatch1 = 0.0;
+  float scaleHatch2 = 0.0;
+
+  if (shading <= stepSize) {
+    alpha = 3.0 * shading;
+    scaleHatch1 = alpha;
+    scaleHatch2 = 1.0 - alpha;
+  }
+  else if (shading > stepSize && shading <= 2.0 * stepSize) {
+    alpha = 3.0 * (shading - stepSize);
+    scaleHatch0 = alpha;
+    scaleHatch1 = 1.0 - alpha;
+  }
+  else if (shading > 2.0 * stepSize) {
+    alpha = 3.0 * (shading - stepSize * 2.0);
+    scaleWhite = alpha;
+    scaleHatch0 = 1.0 - alpha;
+  }
+
+  shadingFactor = scaleWhite + 
+    scaleHatch0 * texture2D(hatch0, uv).r +
+    scaleHatch1 * texture2D(hatch1, uv).r +
+    scaleHatch2 * texture2D(hatch2, uv).r;
+
+  return shadingFactor;
+}
+
 void main() {
-    gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+    vec2 uv = vUv * 15.0;
+    vec2 uv2 = vUv.yx * 10.0;
+    float shading = texture2D(texture, vUv).r;
+    float crossedShading = shade(shading, uv) * shade(shading, uv2) * 0.6 + 0.4;
+    gl_FragColor = vec4(vec3(crossedShading), 1.0);
 }
--- src/shader/cube.vs
+++ src/shader/cube.vs
@@ -1,11 +1,14 @@
 attribute vec4 a_position;
-attribute vec4 a_color;
-varying vec4 v_color;
+// attribute vec4 a_color;
+// varying vec4 v_color;
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_perspective;
+varying vec2 vUv;
+attribute vec2 uv;
 
 void main() {
     gl_Position = u_perspective * u_view * u_model * a_position;
-    v_color = a_color;
+    // v_color = a_color;
+    vUv = uv;
 }
